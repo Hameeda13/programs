7d1e1d6131d1b1ddb09a79588689778e7af9f3d7
Hameeda13 <hameeda_khan13@yahoo.co.in>
2020-08-24 12:46:59 +0530

    ruby_code

---
+++ rubymadg.rb
@@ -0,0 +1,20 @@
+ 
+  puts "Enter a color:"
+
+  color = gets.chomp()
+
+puts "Enter a plural_noun:"
+
+plural_noun = gets.chomp()
+
+puts "Enter a celebiaty:"
+
+celebrity = gets.chomp()
+
+
+puts (" Roses are "+color) 
+
+puts (" Violets are "+ plural_noun)
+
+puts (" I love "+ celebrity)
+
---
+++ test.rb
@@ -0,0 +1,14 @@
+  puts "This is main ruby program"
+ 
+
+END {
+     puts "Terminating ruby program"
+}
+
+
+
+
+BEGIN {
+          puts "Initialize ruby program"  }
+
+
